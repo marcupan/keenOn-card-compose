c42e226d092a51694491f29f9b593d759db566f9
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2024-12-05 21:50:09 +0200

    add logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use crate::config::Config;
 use compose::compose_service_server::{ComposeService, ComposeServiceServer};
 use compose::{ComposeRequest, ComposeResponse};
 use dotenv::dotenv;
+use env_logger;
+use log::{error, info};
 use tonic::{transport::Server, Request, Response, Status};
 use tonic_reflection::server::Builder;
 
@@ -24,10 +26,14 @@ impl ComposeService for MyComposeService {
 
         match services::image_service::compose_image_with_text(&req.image_base64, &req.text) {
             Ok(composed_image) => {
+                info!("Successfully composed image");
                 let reply = ComposeResponse { composed_image };
                 Ok(Response::new(reply))
             }
-            Err(e) => Err(Status::internal(format!("Failed to compose image: {}", e))),
+            Err(e) => {
+                error!("Failed to compose image: {}", e);
+                Err(Status::internal(format!("Failed to compose image: {}", e)))
+            }
         }
     }
 }
@@ -35,10 +41,11 @@ impl ComposeService for MyComposeService {
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    env_logger::init();
 
     let config = Config::from_env();
 
-    println!("API Key: {}", config.api_key);
+    info!("API Key: {}", config.api_key);
 
     let addr = format!("0.0.0.0:{}", config.port).parse()?;
     let compose_service = MyComposeService;
@@ -47,6 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .register_encoded_file_descriptor_set(compose::FILE_DESCRIPTOR_SET)
         .build_v1()?;
 
+    info!("Starting server on address: {}", addr);
     Server::builder()
         .add_service(ComposeServiceServer::new(compose_service))
         .add_service(reflection_service)
